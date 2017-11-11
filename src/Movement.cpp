4f9680fdc8b834a738e5bbad667ddf77e66be29a
Canis (Joshua Jacobson) <linktheherooftime2@gmail.com>
2017-11-11 00:21:01 -0700

    Added DriveTrain safety back. Again.
    Uploaded from an outdated codebase.
    Creating a new branch now.

--- src/Movement.cpp
+++ src/Movement.cpp
@@ -30,7 +30,6 @@ void Movement::init(int frontLeftPort, int frontRightPort, int backLeftPort, int
 
 	if (drive_ != nullptr) delete drive_;
 	drive_ = new frc::RobotDrive(frontLeft, backLeft, frontRight, backRight);
-	drive_->SetSafetyEnabled(false);
 
 	if (ARCADE_DRIVE != nullptr) delete ARCADE_DRIVE;
 	ARCADE_DRIVE = new ArcadeDrive(drive_, controller_);
@@ -53,7 +52,6 @@ Movement::Movement(int frontLeftPort, int frontRightPort, int backLeftPort, int 
 	backRight = new Talon(backRightPort);
 
 	drive_ = new frc::RobotDrive(frontLeftPort, backLeftPort, frontRightPort, backRightPort);
-	drive_->SetSafetyEnabled(false);
 
 	NULL_DRIVE = new NullDrive();
 	ARCADE_DRIVE = new ArcadeDrive(drive_, controller_);
